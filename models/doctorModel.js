c7f246c2003ec39a88e0e5a18e1003be322fb935
Komal <albhar.k@northeastern.edu>
2023-04-13 13:45:20 -0400

    Models Created

---
+++ models/doctorModel.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const doctorSchema = new mongoose.Schema(
+  {
+    // _id: {
+    //   type: String,
+    //   required: false,
+    // },
+    userId: {
+      type: String,
+      required: false,
+    },
+    // firstName: {
+    //   type: String,
+    //   required: false,
+    // },
+    email: {
+      type: String,
+      required: false,
+    },
+    name: {
+      type: String,
+      required: false,
+    },
+    phoneNumber: {
+      type: String,
+      required: false,
+    },
+    website: {
+      type: String,
+      required: false,
+    },
+    password: {
+      type: String,
+      required: false,
+    },
+    address: {
+      type: String,
+      required: false,
+    },
+    specialization: {
+      type: String,
+      required: false,
+    },
+    experience: {
+      type: String,
+      required: false,
+    },
+    feePerCunsultation: {
+      type: Number,
+      required: false,
+    },
+    timings: {
+      type: Array,
+      required: false,
+    },
+    status: {
+      type: String,
+      default: "pending",
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const doctorModel = mongoose.model("doctors", doctorSchema);
+module.exports = doctorModel;
